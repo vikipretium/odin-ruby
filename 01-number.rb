23ed1a269c9d43752b7f98151c0fc688062ff93b
vikipretium <vighneshpasupathy@gmail.com>
2023-02-11 07:18:32 +0530

    numbers

---
+++ 01-number.rb
@@ -0,0 +1,13 @@
+#addition
+puts "2 + 1 = " + "#{2+1}"  
+#subtraction
+puts "2 - 1 = " + "#{2-1}"
+#mul
+puts "2 * 2 = " + "#{2*2}"
+#div
+puts "10/5 = " + "#{10/5}"
+#Exponent
+puts "2 ** 2 = " + "#{2**2}"
+#modulus
+puts "10%3 = " + "#{10%3}"
+
